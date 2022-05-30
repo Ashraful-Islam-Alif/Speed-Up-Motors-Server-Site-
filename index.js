5663f62ce805821119815607a8c3ede1c0066f19
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-05-30 21:15:42 +0600

    admin role

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 app.use(cors());
 app.use(express.json());
@@ -33,6 +34,7 @@ async function run() {
         await client.connect();
         const toolCollection = client.db('speedUp_Motors').collection('tools');
         const myOrderCollection = client.db('speedUp_Motors').collection('myOrder');
+        const paymentCollection = client.db('speedUp_Motors').collection('payment');
         const userCollection = client.db('speedUp_Motors').collection('user');
 
         //Auth(JWT)
@@ -43,6 +45,19 @@ async function run() {
             });
             res.send({ accessToken });
         })
+
+        //payment
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
 
 
         //find all tools items
@@ -83,32 +98,52 @@ async function run() {
             const orders = await myOrderCollection.findOne(query)
             res.send(orders);
         })
-        // app.put('/myOrder/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const user = req.body;
-        //     const filter = { email: email };
-        //     const options = { upsert: true };
-        //     const updateDoc = {
-        //         $set: user,
-        //     };
-        //     const result = await myOrderCollection.updateOne(filter, updateDoc, options);
-        //     const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-        //     res.send({ result, token });
-        // });
+
+        //Payment method
+        app.patch('/myOrder/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+
+            const result = await paymentCollection.insertOne(payment);
+            const updatedOrder = await myOrderCollection.updateOne(filter, updatedDoc);
+            res.send(updatedDoc);
+        })
+
 
         app.get('/user', async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
         });
-
-        app.put('/user/admin/:email', async (req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
         });
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
